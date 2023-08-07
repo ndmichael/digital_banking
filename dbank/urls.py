907af7271b365ce72e8c1d61eea505042c127dcd
ndmichael <ukejemicheal@gmail.com>
2023-08-07 16:15:56 +0100

    add the url route for visa gold page

--- dbank/urls.py
+++ dbank/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, about, investments
+from .views import index, about, investments, visagold
 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', index, name="index"),
     path('about/', about, name="about"),
     path('investments/', investments, name="investments"),
+    path('virtualcards/visagold', visagold, name="vcard"),
 ]
