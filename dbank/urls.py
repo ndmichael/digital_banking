6b91d13983a4eb299ba7c7114839b9ae89047893
ndmichael <ukejemicheal@gmail.com>
2023-08-10 11:15:49 +0100

    Added a view routes for visa platinum

--- dbank/urls.py
+++ dbank/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', index, name="index"),
     path('about/', about, name="about"),
     path('investments/', investments, name="investments"),
-    path('virtualcards/visagold', visagold, name="vcard"),
-    path('virtualcards/visainfinite', visainfinite, name="vcard"),
+    path('virtualcards/visagold', visagold, name="visagold"),
+    path('virtualcards/visainfinite', visainfinite, name="visaInfinite"),
+    path('virtualcards/visaplatinum', visainfinite, name="visaInfinite"),
     path('banking/accounts/types', bank_account_types, name="baccounts"),
 ]
