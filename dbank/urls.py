dd88be049c628e483fa9e3157c6a36c39a87da9b
ndmichael <ukejemicheal@gmail.com>
2023-08-10 10:22:59 +0100

    url for bank/account/types

--- dbank/urls.py
+++ dbank/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, about, investments, visagold, visainfinite
+from .views import index, about, investments, visagold, visainfinite, bank_account_types
 
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('investments/', investments, name="investments"),
     path('virtualcards/visagold', visagold, name="vcard"),
     path('virtualcards/visainfinite', visainfinite, name="vcard"),
+    path('banking/accounts/types', bank_account_types, name="baccounts"),
 ]
