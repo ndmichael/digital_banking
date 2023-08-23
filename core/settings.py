41b881e314859c289dbb8109ee3fcdc2d98037fd
ndmichael <ukejemicheal@gmail.com>
2023-08-23 14:44:15 +0100

    setup postgres in settings

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digital_bank',
+        'USER': 'postgres',
+        'PASSWORD': 'daliMIKE0417',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
