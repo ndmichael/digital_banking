365d51a4eba3449b2f0bf87b3ac5519fac571a9a
ndmichael <ukejemicheal@gmail.com>
2023-08-23 11:28:04 +0100

    django countries was added to installed_apps

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dbank',
+    'clients',
+    'django_countries',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
