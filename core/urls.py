ce305b85cc9e11d0679ff233d2638825f8b5880c
ndmichael <ukejemicheal@gmail.com>
2023-08-24 06:12:58 +0100

    allauth urls added

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dbank.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
