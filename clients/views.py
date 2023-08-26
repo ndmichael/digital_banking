5e14034e6507dd68071193ad18189cd3472c43af
ndmichael <ukejemicheal@gmail.com>
2023-08-26 14:49:34 +0100

    profile view added

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+def profile(request):
+    return render(request, 'users/profile.html')
+
