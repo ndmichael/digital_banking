eb5cb0ac8d29490c2011d74dfa66f8c96a81ace6
ndmichael <ukejemicheal@gmail.com>
2023-08-26 21:42:23 +0100

    models linked to admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Savings, CustomUser, FixedDeposit, Card
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Savings)
+admin.site.register(FixedDeposit)
+admin.site.register(Card)
