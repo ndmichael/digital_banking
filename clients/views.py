77888b9dc188ed4b03f92b0870e65b4fecfcb76e
ndmichael <ukejemicheal@gmail.com>
2023-08-27 02:13:59 +0100

    view capturing data

--- clients/views.py
+++ clients/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
 
 # Create your views here.
 
-def profile(request):
-    return render(request, 'users/profile.html')
+@login_required
+def profile(request, username):
+    user = get_object_or_404(CustomUser, username=username)
+    context = {
+        "user": user
+    }
+    return render(request, 'users/profile.html', context)
 
