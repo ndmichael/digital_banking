8e3b6ceaa1dd0f7d5fc4c224097be7c78d2ab4ee
ndmichael <ukejemicheal@gmail.com>
2023-08-28 01:13:04 +0100

    connect transfer form to the view

--- clients/views.py
+++ clients/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
 from .models import CustomUser
+from .forms import TransferForm
 
 # Create your views here.
 
 @login_required
 def profile(request, username):
+    form = TransferForm()
     user = get_object_or_404(CustomUser, username=username)
     context = {
-        "user": user
+        "user": user,
+        "form": form
     }
     return render(request, 'users/profile.html', context)
 
