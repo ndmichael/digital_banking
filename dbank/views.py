af9f2c9f42b7e92a3b0b64988dc4f169467ea5a4
ndmichael <ukejemicheal@gmail.com>
2023-08-28 01:24:27 +0100

    set up on settings

--- dbank/views.py
+++ dbank/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
+    if request.user.is_authenticated:
+        return redirect("clientprofile", username=request.user.username)
     return render(request, 'dbank/index.html', {'title': 'home'})
 
 
