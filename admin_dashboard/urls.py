35baf69ba46bc5d023854a5f79908fb1ea09c012
ndmichael <ukejemicheal@gmail.com>
2023-08-30 17:30:14 +0100

    route added for dashboard

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
-    admin_dashboard
+    admin_dashboard,
+    register
 )
 
 
 
 urlpatterns = [
     path("", admin_dashboard, name="admindashboard"),
+    path("register/client/", register, name="register"),
 ]
