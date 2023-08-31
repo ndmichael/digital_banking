7aa5194bcf60efe3948588706c546baff1f0838a
ndmichael <ukejemicheal@gmail.com>
2023-08-31 08:45:49 +0100

    all transfer route added

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     admin_dashboard,
     register,
-    all_users
+    all_users,
+    all_transfers
 )
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("", admin_dashboard, name="admindashboard"),
     path("register/", register, name="register"),
     path("users/all/", all_users, name="all_users"),
+    path("transfer/all/", all_transfers, name="all_transfers"),
 ]
