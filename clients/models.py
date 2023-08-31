6eda0cf11a1b16b2f46b6a218b93336aa741ebaf
ndmichael <ukejemicheal@gmail.com>
2023-08-31 08:08:29 +0100

    new transaction db model added

--- clients/models.py
+++ clients/models.py
@@ -164,7 +164,7 @@ class Transfer(models.Model):
         ('pending', 'PENDING'),
         ('failed', 'FAILED')
     )
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="cl_transfer")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="bank_transfer")
     account = models.ForeignKey(Savings, on_delete=models.CASCADE, related_name="acc_transfer")
     currency = models.CharField(max_length=10, default="US DOLLAR")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -179,3 +179,19 @@ class Transfer(models.Model):
 
     def __str__(self):
         return f'{self.receivers_name}'
+
+
+class Transaction(models.Model):
+    RECORD = (
+        ('credit', 'CREDIT'),
+        ('debit', 'DEBIT')
+    )
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="history")
+    record = models.CharField(default='credit', choices=RECORD, max_length=10)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    amt_aft_charges = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    description =models.TextField(null=True, blank=True)
+    transaction_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.transaction_date}"
