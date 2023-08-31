5abab6f840cb5124d7ac9f461c5e155807083255
ndmichael <ukejemicheal@gmail.com>
2023-08-31 14:31:58 +0100

    logic to commit and auto create elite account for clients

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MyCustomSignupForm
-from clients.models import  CustomUser, Transfer
+from clients.models import  CustomUser, Transfer, Savings
+from random import randrange
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,42 +16,46 @@ def register(request):
     #     messages.error(
     #             request, f"You do not have permission to access this page."
     #         )
-    #     return redirect("/")
-    # if request.method == "POST":
-    #     u_form = UserForm(request.POST)
-    #     c_form = ClientRegisterForm(request.POST,  request.FILES)
-    #     if u_form.is_valid() and c_form.is_valid():
-    #         user = u_form.save()
-    #         client = c_form.save(commit=False)
-    #         number = [randrange(10) for i in range(10)]
-    #         acc_number = ''.join(str(i) for i in number)
-    #         pin = acc_number[:4]
-    #         client.user = user
-    #         client.transfer_pin = pin
-    #         client.account_number = acc_number
-    #         client.save()
-    #         username = u_form.cleaned_data.get("username")
+        # return redirect("/")
+    if request.method == "POST":
+        c_form = MyCustomSignupForm(request.POST,  request.FILES)
+        if c_form.is_valid():
+            
+            # fname = 
+            balance = c_form.cleaned_data.get("balance")
+            username = c_form.cleaned_data.get("username")
+            country = c_form.cleaned_data.get("country")
+            
+            user = c_form.save(request)
+            
 
-    #         # EMAILING 
-    #         '''subject = f"Account Creation"
-    #         message = f"""'success', Account has been created for {username}.
-    #             password:....
-    #         """
-    #         sender = "mickeyjayblest@gmail.com"
-    #         send_mail(
-    #             subject,
-    #             message,
-    #             'mickeyjayblest@gmail.com',
-    #             ['ukejemicheal@gmail.com']
-    #         )'''
+            
+            print(f"country: {country} balance: {balance}")
+            number = [randrange(10) for i in range(10)]
+            number = ''.join(str(i) for i in number)
+            pin = number[:4]
 
-    #         messages.success(
-    #             request, f"Account has been created for {username} you  can now login."
-    #         )
-    #         return redirect("all_users")
-    # else:
-    #     u_form = UserForm()
-    c_form = MyCustomSignupForm()
+            Savings.objects.create(user=user, number=number, balance=balance, pin=pin)
+
+            # EMAILING 
+            '''subject = f"Account Creation"
+            message = f"""'success', Account has been created for {username}.
+                password:....
+            """
+            sender = "mickeyjayblest@gmail.com"
+            send_mail(
+                subject,
+                message,
+                'mickeyjayblest@gmail.com',
+                ['ukejemicheal@gmail.com']
+            )'''
+
+            messages.success(
+                request, f"Account has been created for {username} you  can now login."
+            )
+            return redirect("all_users")
+    else:
+        c_form = MyCustomSignupForm()
     context = {
         'c_form': c_form,
         'title': "Registeration"
