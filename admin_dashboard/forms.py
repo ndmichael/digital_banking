93582cb698f5654e5396989d1c2f6cbbf6cbe498
ndmichael <ukejemicheal@gmail.com>
2023-08-31 14:31:24 +0100

    adjust on customloginform

--- admin_dashboard/forms.py
+++ admin_dashboard/forms.py
@@ -18,39 +18,22 @@ gender = (
 )
 
 class MyCustomSignupForm(SignupForm):
-    field_order = ['first_name', 'last_name',  'username',
-                   'email', 'password1', 'password2', 'balance','country', 'address', 'dob','image']
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last name')
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'name@example.com'}))
     balance = forms.DecimalField(max_digits=10, decimal_places=2)
     address = forms.CharField(widget=forms.Textarea(attrs={'name':'body', 'rows':'5'}))
-    country = CountryField(blank=True).formfield()
+    country = CountryField(blank=False).formfield()
     dob = forms.DateField(widget=DateInput)
     gender = forms.ChoiceField(choices=gender)
     image = forms.ImageField(required=False)
 
     def save(self, request):
-        CustomUser = super(MyCustomSignupForm, self).save(request)
-        CustomUser.first_name = self.cleaned_data['first_name']
-        CustomUser.last_name = self.cleaned_data['last_name']
-        CustomUser.save()
-        # number = [randrange(10) for i in range(10)]
-        # acc_number = ''.join(str(i) for i in number)
-        # pin = acc_number[:4]
-        # models.Client.objects.create(
-        #         user=user, 
-        #         balance=self.cleaned_data['balance'], 
-        #         address=self.cleaned_data['address'], 
-        #         country=self.cleaned_data['country'], 
-        #         dob=self.cleaned_data['dob'],
-        #         gender = self.cleaned_data['gender'],
-        #         account_number = acc_number,
-        #         transfer_pin = pin,
-        #         active = True
-
-        # )
-        # return user
+        CustomUser = super(MyCustomSignupForm, self).save(request)     
+        CustomUser.country = self.cleaned_data['country']
+        CustomUser.save() 
+        
+        return CustomUser
 
     def __init__(self, *args, **kwargs):
         super(MyCustomSignupForm, self).__init__(*args, **kwargs)
