ddf86034df295d7c6f3a44842c1ce9b362748033
ndmichael <ukejemicheal@gmail.com>
2023-08-31 07:52:16 +0100

    dbank model left empty

--- dbank/models.py
+++ dbank/models.py
@@ -8,40 +8,6 @@ from django_countries.fields import CountryField
 
 
 
-class Transfer(models.Model):
-    status_choices = (
-        ('success', 'SUCCESS'),
-        ('pending', 'PENDING'),
-        ('failed', 'FAILED')
-    )
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="cl_transfer")
-    account = models.ForeignKey(Savings, on_delete=models.CASCADE, related_name="acc_transfer")
-    currency = models.CharField(max_length=10, default="US DOLLAR")
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    swift_code = models.CharField(max_length = 11, blank=True, null=True)
-    receivers_name = models.CharField(max_length=50)
-    beneficiary_account_number = models.CharField(max_length=20)
-    beneficiary_bank_address = models.TextField()
-    country = CountryField()
-    dotf = models.DateTimeField(default=timezone.now)
-    is_success =  models.CharField(choices=status_choices, default="pending")
-    reference = models.CharField(null=False, blank=False, max_length=15)
-
-    def __str__(self):
-        return f'{self.receivers_name}'
 
 
-class Transaction(models.Model):
-    RECORD = (
-        ('credit', 'CREDIT'),
-        ('debit', 'DEBIT')
-    )
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="history")
-    record = models.CharField(default='credit', choices=RECORD, max_length=10)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    amt_aft_charges = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    description =models.TextField(null=True, blank=True)
-    transaction_date = models.DateField(default=timezone.now)
 
-    def __str__(self):
-        return f"{self.transaction_date}"
