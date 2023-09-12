3ea24d2b2a1c704178a64ebe2a99de30a0acf6eb
ndmichael <ukejemicheal@gmail.com>
2023-09-12 20:57:12 +0100

    route for add transaction

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -7,6 +7,7 @@ from .views import (
     update_user,
     load_balance,
     historypage,
+    addtransaction,
 )
 
 
@@ -17,6 +18,7 @@ urlpatterns = [
     path("users/all/", all_users, name="all_users"),
     path("transfer/all/", all_transfers, name="all_transfers"),
     path("user/history/", historypage, name="historypage"),
+    path("user/history/add/<str:username>/", addtransaction, name="addtransaction"),
     path("update/user/<str:username>/", update_user, name="update_user"),
     path("loadbalance/<str:username>/", load_balance, name="loadbalance"),
 ]
