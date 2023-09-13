76bd13102becd204df8e6aabe0f28257a4bd0d03
ndmichael <ukejemicheal@gmail.com>
2023-09-13 13:30:39 +0100

    redirect to profile url added

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import profile
+from .views import profile, current_user_profile
 
 
 
 
 urlpatterns = [
     path('profile/<str:username>', profile , name="clientprofile"),
+    path('', current_user_profile, name="currentprofile")
 ]
