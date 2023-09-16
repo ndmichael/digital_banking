30261dcdb5a208a1fdd3fba891dcc8a3ca85bd58
ndmichael <ukejemicheal@gmail.com>
2023-09-16 17:23:50 +0100

    boto3 storages added for s3

--- core/settings.py
+++ core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     #crispy forms
     "crispy_forms",
     "crispy_bootstrap5",
+    "storages",  
 ]
 
 MIDDLEWARE = [
@@ -189,3 +190,19 @@ ACCOUNT_FORMS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# setting environmental variables for S3
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+AWS_DEFAULT_UCL = None
+
+# if not DEBUG:
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
