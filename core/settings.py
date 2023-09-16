a8b4ee5b9e472513adae311b0a94943d83facc8e
ndmichael <ukejemicheal@gmail.com>
2023-09-16 13:10:14 +0100

    migration completed

--- core/settings.py
+++ core/settings.py
@@ -88,18 +88,33 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'digitalbank',
-        'USER': 'postgres',
-        'PASSWORD': 'daliMIKE0417',
-        'HOST': 'localhost',
-        'PORT': 5432,
+if DEBUG:
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': BASE_DIR / 'db.sqlite3',
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'digitalbank',
+                'USER': env('PGUSER'),
+                'PASSWORD': env('LOCALPW'),
+                'HOST': 'localhost',
+                'PORT': 5432
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': env('PGUSER'),
+            'PASSWORD': env('PGPASSWORD'),
+            'HOST': env('PGHOST'),
+            'PORT': env('PGPORT'),
+        }
+    }
+
 
 AUTH_USER_MODEL = "clients.CustomUser"
 
