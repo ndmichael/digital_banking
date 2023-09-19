0b50ac8b881dbc3b249ca5160dabae06c999ec61
ndmichael <ukejemicheal@gmail.com>
2023-09-19 13:26:43 +0100

    pushed

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-            # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
             # 'NAME': 'digitalbank',
             # 'USER': "postgres",
             # 'PASSWORD': env('LOCALPW'),
