71005e157229908110147f7f9ccee8ca14d713d3
ndmichael <ukejemicheal@gmail.com>
2023-11-27 14:33:13 +0100

    routes added

--- clients/urls.py
+++ clients/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import profile, current_user_profile
+from .views import profile, current_user_profile, card_request
 
 
 
 
 urlpatterns = [
     path('profile/<str:username>', profile , name="clientprofile"),
-    path('', current_user_profile, name="currentprofile")
+    path('', current_user_profile, name="currentprofile"),
+    path('user/cards/request', card_request, name="card_request"),
 ]
