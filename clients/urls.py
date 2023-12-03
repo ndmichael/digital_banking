39df533ae8b1c280abb221592ec93909cedaba1d
ndmichael <ukejemicheal@gmail.com>
2023-12-03 13:41:49 +0100

    activate back card request

--- clients/urls.py
+++ clients/urls.py
@@ -10,5 +10,5 @@ from .views import (
 urlpatterns = [
     path('profile/<str:username>', profile , name="clientprofile"),
     path('', current_user_profile, name="currentprofile"),
-    # path('user/cards/request', card_request, name="card_request"),
+    path('user/cards/request', card_request, name="card_request"),
 ]
